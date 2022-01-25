5efb5dfc21171f94fe59822da541f64fe5906c84
Augusto Comegllio <69863123+AugustoComeglio@users.noreply.github.com>
2022-01-25 01:05:16 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -10,23 +10,25 @@ using namespace std;
 int main() {
 	
 	char clave[50000];
-	vector<int> posiciones;
+	vector<int> posiciones;//Vector con las posiciones de los 5 asteriscos
 	cout<< "Digite su clave cifrada: ";
-	cin.getline(clave,50000,'\n');
-	ofstream archivo;
-	archivo.open("CLAVE.OUT.txt",ios::out);//abrir el archivo
-	int digitoClave = 4;
+	cin.getline(clave,50000,'\n');//Guardo en clave el la cadena ingresada
+	ofstream archivo;//Creo archivo
+	archivo.open("CLAVE.OUT.txt",ios::out);//Genera el archivo .txt 
+	int digitoClave = 4;//cantidad de digitos de la clave
 	
+	//Recorro la cadena para sacar las posiciones de los asteriscos
 	for (int i = 0; i<strlen(clave); i++){
 		if (clave[i] == '*'){
-			posiciones.insert(posiciones.end(), i);
+			posiciones.insert(posiciones.end(), i); //inserto en el vector de posicones la posicion de cada asterisco
 		}
 	}
 	cout<<"Clave descifrada"<<endl;
+	//Recorro el vector de posiciones para cacular cuantos caracteres hay entre dos asteriscos
 	for (int j = 0; j<digitoClave; j++){
-		int resultado = posiciones[j+1] - posiciones[j] - 1;
-		//cout<<resultado<<endl;
-		archivo<<resultado<<endl;
+		int resultado = posiciones[j+1] - posiciones[j] - 1; 
+		//cout<<resultado<<endl;//Escribe en consola
+		archivo<<resultado<<endl;//Escribe en el archivo .txt que se genera
 	}
 	return 0;
 }
